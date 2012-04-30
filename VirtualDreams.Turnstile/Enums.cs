1b570b5e424b2f28c9fd8fe59c63312bff6d1c63
Gilbert Corrales <gilbert@cometoide.com>
2012-04-29 20:30:37 -0600

    - initial commit from current state of http://turnstile.codeplex.com

---
+++ VirtualDreams.Turnstile/Enums.cs
@@ -0,0 +1,20 @@
+﻿namespace VirtualDreams.Turnstile
+{
+    public enum EnterMode
+    {
+        Enter,
+        Exit
+    }
+
+    public enum YDirection
+    {
+        TopToBottom,
+        BottomToTop
+    }
+
+    public enum ZDirection
+    {
+        FrontToBack,
+        BackToFront
+    }
+}
